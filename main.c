81c21623a426443191db6c8cc472ea46d7c8904a
Adam Tecle <adam.tecle@gmail.com>
2013-12-05 19:01:59 -0500

    second commit

--- main.c
+++ main.c
@@ -7,9 +7,14 @@
 //
 
 #include "malloc.h"
+#define malloc(x) my_malloc(x)
+#define free(x) my_free(x)
 
 int main(int argc, char **argv) {
     
+    int x;
+    malloc(x);
+    free(x);
     
     return 1;
 }
--- malloc.c
+++ malloc.c
@@ -2,12 +2,15 @@
 #include "malloc.h"
 
 
+
 void *my_malloc(unsigned int size) {
     
-    
+    return NULL;
 }
 
 void *my_free(unsigned int size) {
     
+    
+    return NULL;
     
 }
